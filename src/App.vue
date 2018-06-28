27725de1c8203d8adbfca8ca90ed778362bb0ffc
Jocelyn Jeffrey <jocelynjeffrey@gmail.com>
2018-06-28 10:52:24 -0400

    init commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <Home />
+  </div>
+</template>
+
+<script>
+import Home from './components/Home';
+
+export default {
+  name: 'App',
+  components: {
+    Home,
+  },
+};
+</script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Berkshire+Swash');
+body {
+  margin: 0;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  font-size: 16px;
+  background: no-repeat center url("./assets/imgs/sky.png");
+  background-size: cover;
+  padding: 40px;
+  color:rgb(245, 210, 15);
+}
+
+h1 {
+  margin: 0;
+  padding: 1.5rem 0 2.5rem;
+  font-family: 'Berkshire Swash', cursive;
+  text-shadow: -1px 0 #AD3237, 0 1px #AD3237, 1px 0 #AD3237, 0 -1px #AD3237;
+  font-size: 8rem;
+}
+
+h2 {
+  font-size: 3rem;
+}
+
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>{{ main }}</h1>
+    <img src="../assets/imgs/crystal_ball.png">
+    <h2>{{ fortune }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data() {
+    return {
+      main: 'Ask Zandar',
+      fortune: 'What will your fortune reveal?',
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+img {
+  width: 80%;
+  max-width: 500px;
+}
+</style>
